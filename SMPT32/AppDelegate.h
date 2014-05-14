d9c147760e85fea077ddfc9890a2ac4f9b7efdde
FHICT <FHICT@MacBook-van-Systeembeheer.local>
2014-05-14 22:53:46 +0200

    second commit

---
+++ SMPT32/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SMPT32
+//
+//  Created by FHICT on 14-05-14.
+//  Copyright (c) 2014 Fontys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMPT32/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SMPT32
+//
+//  Created by FHICT on 14-05-14.
+//  Copyright (c) 2014 Fontys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
